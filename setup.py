0ed3135ec66cdd33a7d8c498f3405c18cf9be773
jonathan vanasco <jonathan@2xlp.com>
2021-01-22 13:28:19 -0500

    update tests/setup

--- setup.py
+++ setup.py
@@ -3,25 +3,30 @@ import re
 from setuptools import setup
 from setuptools import find_packages
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+long_description = (
+    description
+) = "twython wrapper to simplify tweeting from the commandline"
+with open(os.path.join(HERE, "README.md")) as r_file:
+    long_description = r_file.read()
+
 # store version in the init.py
-with open(os.path.join(os.path.dirname(__file__), "tweet_helper.py")) as v_file:
+with open(os.path.join(HERE, "tweet_helper.py")) as v_file:
     VERSION = (
         re.compile(r".*__VERSION__ = \"(.*?)\"", re.S).match(v_file.read()).group(1)
     )
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = ""
-README = README.split("\n\n", 1)[0] + "\n"
 
 setup(
     name="tweet_helper",
     author="Jonathan Vanasco",
     author_email="jonathan@findmeon.com",
     version=VERSION,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/jvanasco/tweet_helper",
     py_modules=["tweet_helper"],
-    description="twython wrapper to simplify tweeting from the commandline",
-    long_description=README,
     zip_safe=False,
     keywords="",
     test_suite="tests",
